d2e8dd4d09dab23cb25e687be0a2c90208248b22
NeilNeel <neelbarvaliya04@gmail.com>
2025-07-07 18:34:24 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/NeilNeel/lenslocked
+
+go 1.23.0
+
+require github.com/go-chi/chi/v5 v5.2.2
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Hello World!!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, please email me at <a href=\"barvaliyaneel010@gmail.com\">barvaliyaneel010@gmail.com</a></p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "<p>Q: Is there a free version?</p>")
+	fmt.Fprintf(w, "<p>A: Yes! We offer a free trail for 30 days on any paid plans.</p>")
+	fmt.Fprintf(w, "<hr/>")
+	fmt.Fprintf(w, "<p>Q: What's the most memorable show you watched?</p>")
+	fmt.Fprintf(w, "<p>A: Hmm, I think it would be Mr. Plankton</p>")
+	fmt.Fprintf(w, "<hr/>")
+	fmt.Fprintf(w, "<p>Q: How can I contact you?</p>")
+	fmt.Fprintf(w, "<p>A: you can mail me at <a href=\"mailto: barvaliyaneel@gmail.com\">barvaliyaneel@gmail.com</a></p>")
+}
+
+type Router struct {}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	switch r.URL.Path{
+	case "/":
+		homeHandler(w,r)
+	case "/contact":
+		contactHandler(w,r)
+	case "/FAQ":
+		faqHandler(w,r)
+	default:
+		http.Error(w, "Page not Found", http.StatusNotFound)
+	}
+}
+
+
+func main(){
+	r := chi.NewRouter()
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
+}
