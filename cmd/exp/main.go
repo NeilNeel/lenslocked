f5a3d299e3649f8cc6fb6bc72a1c479d2e21929e
NeilNeel <neelbarvaliya04@gmail.com>
2025-07-10 16:05:40 -0400

    cross site scripting example

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -5,19 +5,24 @@ import (
 	"os"
 )
 
+type User struct{
+	Name string
+	Bio string
+}
+
 func main(){
 
-	user := struct{
-		Name string
-	}{
-		Name: "neel",
-	}
+	var user User
+
+	user.Name = "Neel"
+	user.Bio = "alert(\"Haha, you have been h4x0r3d!\")"
 
 	// var tmpl = "hello.gohtml"
 	tmpl, err := template.ParseFiles("hello.gohtml")
 	if err!= nil {
 		panic(err)
 	}
+
 	err = tmpl.Execute(os.Stdout, user)
 	if err!= nil {
 		panic(err)
